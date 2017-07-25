48991951decf12b75341e399dd3e5b5ac0555793
anhtuan <leekirby66789@gmail.com>
2017-07-25 22:37:01 +0700

    feat(ngModel): The Hero Editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <h3>
+  {{title}}
+  <br>
+  <hr>
+  <input [(ngModel)]="hero.name" placeholder="name">
+  <br>
+  <label>sau khi binding:
+  </label>
+  <br>
+  {{hero.name}}
+  </h3>
+  `
+
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Two-way binding with ngModel';
+  hero: Hero = {
+    id: 1,
+    name: 'danger'
+  }
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
