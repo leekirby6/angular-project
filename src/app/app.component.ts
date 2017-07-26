8dc2629a7fabff282f23747e1704725f3007aa8e
anhtuan <leekirby66789@gmail.com>
2017-07-26 16:01:38 +0700

    replace source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,32 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template: `
-  <h3>
-  {{title}}
-  <br>
-  <hr>
-  <input [(ngModel)]="hero.name" placeholder="name">
-  <br>
-  <label>sau khi binding:
-  </label>
-  <br>
-  {{hero.name}}
-  </h3>
-  `
-
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Two-way binding with ngModel';
-  hero: Hero = {
-    id: 1,
-    name: 'danger'
-  }
+  title = 'app';
 }
-
-export class Hero {
-  id: number;
-  name: string;
-}
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,7 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
